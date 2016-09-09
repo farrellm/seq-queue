c30b78c327ed2c997f0c903f27b482c559f9c325
Matthew Farrell <farrellm@alum.mit.edu>
2016-09-09 00:53:32 -0400

    mono-traversable instances

--- src/Data/Sequence/Queue.hs
+++ src/Data/Sequence/Queue.hs
@@ -1,31 +1,34 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE DeriveTraversable #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving, FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
 
 module Data.Sequence.Queue
   (Queue(..)
-  ,empty
-  ,singleton
   ,(|>)
   ,(><)
-  ,null
-  ,length
   ,ViewQ(..)
   ,deq
   ) where
 
-import           Prelude hiding (null, length)
+import Prelude
+       hiding (null, length, replicate, filter, splitAt, take, drop,
+               break, span, dropWhile, takeWhile)
 import           Control.Applicative (Alternative)
 import           Control.Monad (MonadPlus)
 import qualified Data.Sequence as S
-import           GHC.Exts
+-- import           GHC.Exts
 import           Data.String
 import           Data.Data
+import           Data.MonoTraversable
+import           Data.Sequences
 
 newtype Queue a =
   Queue {getQueue :: S.Seq a}
   deriving (Read,Show,Data,Eq,Ord,Monoid,
+            Foldable,Traversable,
             Functor,Applicative,Monad,Alternative,MonadPlus)
 -- IsList,NFData,IsString
 -- deriving instance IsString (Queue (S.Seq Char))
@@ -34,13 +37,89 @@ newtype Queue a =
 --   type Item (Queue (S.Seq a)) = a
 --   fromList l = Queue (fromList l)
 
-empty :: Queue a
-empty = Queue S.empty
-{-# INLINE empty #-}
+type instance Element (Queue a) = a
 
-singleton :: a -> Queue a
-singleton v = Queue (S.singleton v)
-{-# INLINE singleton #-}
+deriving instance MonoFoldable (Queue a)
+deriving instance MonoFunctor (Queue a)
+instance MonoTraversable (Queue a)
+
+-- Applicative
+instance MonoPointed (Queue a)
+
+instance GrowingAppend (Queue a)
+
+instance SemiSequence (Queue a) where
+    type Index (Queue a) = Int
+    cons a (Queue q) = Queue (a S.<| q)
+    {-# INLINE cons #-}
+    snoc (Queue q) a = Queue (q S.|> a)
+    {-# INLINE snoc #-}
+    reverse (Queue q) = Queue (S.reverse q)
+    {-# INLINE reverse #-}
+    sortBy f (Queue q) = Queue (S.sortBy f q)
+    {-# INLINE sortBy #-}
+
+    intersperse a (Queue q) = Queue (intersperse a q)
+    {-# INLINE intersperse #-}
+    find a (Queue q) = find a q
+    {-# INLINE find #-}
+
+queuePair :: (S.Seq a, S.Seq b) -> (Queue a, Queue b)
+queuePair (a, b) = (Queue a, Queue b)
+{-# INLINE queuePair #-}
+
+queueWrap :: (S.Seq a -> S.Seq b) -> Queue a -> Queue b
+queueWrap f (Queue q) = Queue (f q)
+{-# INLINE queueWrap #-}
+
+queueWrapPair :: (S.Seq a -> (S.Seq b, S.Seq c))
+              -> Queue a
+              -> (Queue b, Queue c)
+queueWrapPair f (Queue q) = queuePair (f q)
+{-# INLINE queueWrapPair #-}
+
+instance IsSequence (Queue a) where
+    fromList = Queue . fromList
+    replicate n a = Queue (replicate n a)
+    replicateM n a = Queue <$> replicateM n a
+    filter p = queueWrap (filter p)
+    --filterM = filterM
+    break p = queueWrapPair (break p)
+    span p = queueWrapPair (span p)
+    dropWhile p = queueWrap (dropWhile p)
+    takeWhile p = queueWrap (takeWhile p)
+    splitAt n = queueWrapPair (splitAt n)
+    take n = queueWrap (take n)
+    drop n = queueWrap (drop n)
+    partition p = queueWrapPair (partition p)
+    uncons (Queue q) = (\(x, xs) -> (x, Queue xs)) <$> uncons q
+    unsnoc (Queue q) = (\(xs, x) -> (Queue xs, x)) <$> unsnoc q
+    --groupBy = Seq.groupBy
+    tailEx = queueWrap tailEx
+    initEx = queueWrap initEx
+    {-# INLINE fromList #-}
+    {-# INLINE break #-}
+    {-# INLINE span #-}
+    {-# INLINE dropWhile #-}
+    {-# INLINE takeWhile #-}
+    {-# INLINE splitAt #-}
+    {-# INLINE take #-}
+    {-# INLINE drop #-}
+    {-# INLINE partition #-}
+    {-# INLINE uncons #-}
+    {-# INLINE unsnoc #-}
+    {-# INLINE filter #-}
+    {-# INLINE replicate #-}
+    {-# INLINE replicateM #-}
+    {-# INLINE tailEx #-}
+    {-# INLINE initEx #-}
+    index (Queue q) n = index q n
+    indexEx (Queue q) n = indexEx q n
+    unsafeIndex (Queue q) n = unsafeIndex q n
+    {-# INLINE index #-}
+    {-# INLINE indexEx #-}
+    {-# INLINE unsafeIndex #-}
+
 
 (|>) :: Queue a -> a -> Queue a
 (Queue q) |> v = Queue (q S.|> v)
@@ -49,14 +128,6 @@ singleton v = Queue (S.singleton v)
 (><) :: Queue a -> Queue a -> Queue a
 (Queue p) >< (Queue q) = Queue (p S.>< q)
 {-# INLINE (><) #-}
-
-null :: Queue a -> Bool
-null (Queue q) = S.null q
-{-# INLINE null #-}
-
-length :: Queue a -> Int
-length (Queue q) = S.length q
-{-# INLINE length #-}
 
 data ViewQ a = EmptyQ | a :< (Queue a)
 
